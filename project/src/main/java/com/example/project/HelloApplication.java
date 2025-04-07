787d67847c8c72166bc4702c183b19900ff28c95
antoine <azimatter@gmail.com>
2025-04-07 20:25:58 +0200

    debut fxml 1.1

--- project/src/main/java/com/example/project/HelloApplication.java
+++ project/src/main/java/com/example/project/HelloApplication.java
@@ -12,7 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Ruffinary");
         stage.setScene(scene);
         stage.show();
 
--- project/src/main/java/com/example/project/HelloController.java
+++ project/src/main/java/com/example/project/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 public class HelloController {
     @FXML
@@ -10,5 +12,27 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+    @FXML
+    private Button btnAffiche;
+    @FXML
+    private HBox optionBar;
+
+    @FXML
+    public void initialize() {
+        optionBar.setVisible(false);
+        optionBar.setManaged(false);
+
+        btnAffiche.setOnAction(event -> {
+
+            System.out.println("Button clicked");
+
+
+            boolean currentlyVisible = optionBar.isVisible();
+            optionBar.setVisible(!currentlyVisible);
+            optionBar.setManaged(!currentlyVisible);
+        });
     }
 }
